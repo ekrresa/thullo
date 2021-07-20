d68637a786795205e132373911c2b729fa19b148
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-07-20 20:50:20 +0100

    First run

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
