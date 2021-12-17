f4c7a1f919f9126475d1dfb7c57763d9f4a6d026
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-12-17 17:21:44 +0100

    Add classname prop to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,16 +1,17 @@
-import { Fragment, PropsWithChildren, useState } from 'react';
+import { Fragment, PropsWithChildren } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
 interface ModalProps extends PropsWithChildren<unknown> {
   isOpen: boolean;
   closeModal: () => void;
+  className: string;
 }
 
-export function Modal({ children, closeModal, isOpen }: ModalProps) {
+export function Modal({ children, className, closeModal, isOpen }: ModalProps) {
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
-        className="fixed flex justify-center items-baseline inset-0 z-10 overflow-y-auto"
+        className="fixed inset-0 z-10 flex items-baseline justify-center overflow-y-auto "
         onClose={closeModal}
       >
         <Transition.Child
@@ -22,13 +23,8 @@ export function Modal({ children, closeModal, isOpen }: ModalProps) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Dialog.Overlay className="bg-gray-800 opacity-20 fixed inset-0" />
+          <Dialog.Overlay className="fixed inset-0 bg-gray-800 opacity-20" />
         </Transition.Child>
-
-        {/* This element is to trick the browser into centering the modal contents. */}
-        {/* <span className="inline-block h-screen align-middle" aria-hidden="true">
-          &#8203;
-        </span> */}
 
         <Transition.Child
           as={Fragment}
@@ -39,7 +35,9 @@ export function Modal({ children, closeModal, isOpen }: ModalProps) {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <div className="inline-block w-full max-w-md p-6 mb-8 mt-24 overflow-hidden transition-all transform bg-white shadow-xl rounded-2xl">
+          <div
+            className={`inline-block w-full p-6 mt-24 mb-8 overflow-hidden transition-all transform bg-white shadow-xl rounded-2xl ${className}`}
+          >
             {children}
           </div>
         </Transition.Child>
