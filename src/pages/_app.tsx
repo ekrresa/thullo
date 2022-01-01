37c23378583f4db44888f967b8bd7ccb39852da5
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2022-01-01 20:52:32 +0100

    Installed react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { Toaster } from 'react-hot-toast';
 
 import type { Page } from '../types/app';
@@ -11,19 +13,24 @@ type Props = AppProps & {
   Component: Page;
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } },
+});
+
 export default function MyApp({ Component, pageProps }: Props) {
   const renderLayout = Component.getLayout ?? (page => page);
   const isComponentProtected = Boolean(Component.protected);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {isComponentProtected ? (
         <Auth>{renderLayout(<Component {...pageProps} />)}</Auth>
       ) : (
         renderLayout(<Component {...pageProps} />)
       )}
+      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster toastOptions={{ duration: 3000 }} />
-    </>
+    </QueryClientProvider>
   );
 }
 
