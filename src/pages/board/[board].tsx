c2da7e89c1fb26db1a10b56679a5c6f0a31c61c8
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2022-01-07 21:14:00 +0100

    Restructure side menu

--- src/pages/board/[board].tsx
+++ src/pages/board/[board].tsx
@@ -1,7 +1,6 @@
 import React, { ComponentType, useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
-import { IoEllipsisHorizontalSharp } from 'react-icons/io5';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 import { Layout } from '@components/common/Layout';
@@ -33,67 +32,61 @@ export default function Board() {
   }
 
   return (
-    <section className="container relative overflow-hidden mt-9">
+    <>
       {board.data?.body && (
-        <SideMenu
-          open={sideMenu}
-          closeSideMenu={() => setSideMenu(false)}
-          board={board.data.body}
-        />
-      )}
-      <TaskDetails />
+        <section className="container relative overflow-hidden mt-9">
+          <TaskDetails />
 
-      <div className="flex justify-between">
-        <div className="flex items-center">
-          <VisibilitySwitch />
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <VisibilitySwitch
+                boardId={board.data.body.id}
+                defaultVisibility={board.data?.body.visibility}
+              />
 
-          <div className="flex items-center ml-4 mr-4 space-x-4">
-            <div className="relative w-8 h-8 overflow-hidden rounded-lg bg-corn-blue">
-              {board.data?.body && board.data?.body.owner.image_id ? (
-                <Image
-                  src={getCloudinaryUrl(
-                    board.data?.body.owner.image_id as string,
-                    board.data?.body.owner.image_version as string
+              <div className="flex items-center ml-4 mr-4 space-x-4">
+                <div className="relative w-8 h-8 overflow-hidden rounded-lg bg-corn-blue">
+                  {board.data?.body && board.data?.body.owner.image_id ? (
+                    <Image
+                      src={getCloudinaryUrl(
+                        board.data?.body.owner.image_id as string,
+                        board.data?.body.owner.image_version as string
+                      )}
+                      layout="fill"
+                      alt=""
+                    />
+                  ) : (
+                    <div className="w-full h-full">
+                      {getInitials(board.data?.body.owner.name as string)}
+                    </div>
                   )}
-                  layout="fill"
-                  alt=""
-                />
-              ) : (
-                <div className="w-full h-full">
-                  {getInitials(board.data?.body.owner.name as string)}
                 </div>
-              )}
+              </div>
+
+              <BoardInvite />
             </div>
+
+            <SideMenu board={board.data.body} />
           </div>
 
-          <BoardInvite />
-        </div>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <section className="flex items-start p-8 mt-6 space-x-12 overflow-x-auto bg-off-white2 rounded-xl">
+              <Column title="Backlog">
+                <Task id="1" image />
+                <Task id="2" />
+              </Column>
+              <Column title="In Progress">
+                <Task id="3" />
+                <Task id="4" image />
+                <Task id="5" image />
+              </Column>
 
-        <button
-          className="flex items-center px-3 py-2 text-sm rounded-lg bg-off-white text-gray3"
-          onClick={() => setSideMenu(true)}
-        >
-          <IoEllipsisHorizontalSharp className="mr-2" />
-          Show Menu
-        </button>
-      </div>
-
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <section className="flex items-start p-8 mt-6 space-x-12 overflow-x-auto bg-off-white2 rounded-xl">
-          <Column title="Backlog">
-            <Task id="1" image />
-            <Task id="2" />
-          </Column>
-          <Column title="In Progress">
-            <Task id="3" />
-            <Task id="4" image />
-            <Task id="5" image />
-          </Column>
-
-          <AddNewItem text="Add new list" />
+              <AddNewItem text="Add new list" />
+            </section>
+          </DragDropContext>
         </section>
-      </DragDropContext>
-    </section>
+      )}
+    </>
   );
 }
 
