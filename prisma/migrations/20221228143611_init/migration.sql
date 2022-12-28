01e80350f51352014522d07effab19e65864bed3
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2022-12-28 15:36:39 +0100

    Reset migrations

--- prisma/migrations/20221228143611_init/migration.sql
+++ prisma/migrations/20221228143611_init/migration.sql
@@ -1,31 +1,10 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Account`;
-
--- DropTable
-DROP TABLE `Session`;
-
--- DropTable
-DROP TABLE `User`;
-
--- DropTable
-DROP TABLE `VerificationToken`;
-
 -- CreateTable
 CREATE TABLE `accounts` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
     `provider` VARCHAR(191) NOT NULL,
-    `providerAccountId` VARCHAR(191) NOT NULL,
+    `provider_account_id` VARCHAR(191) NOT NULL,
     `refresh_token` TEXT NULL,
     `access_token` TEXT NULL,
     `expires_at` INTEGER NULL,
@@ -35,19 +14,19 @@ CREATE TABLE `accounts` (
     `session_state` VARCHAR(191) NULL,
 
     INDEX `accounts_userId_idx`(`userId`),
-    UNIQUE INDEX `accounts_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    UNIQUE INDEX `accounts_provider_provider_account_id_key`(`provider`, `provider_account_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `sessions` (
     `id` VARCHAR(191) NOT NULL,
-    `sessionToken` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
+    `session_token` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `sessions_sessionToken_key`(`sessionToken`),
-    INDEX `sessions_userId_idx`(`userId`),
+    UNIQUE INDEX `sessions_session_token_key`(`session_token`),
+    INDEX `sessions_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -56,10 +35,10 @@ CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NULL,
-    `emailVerified` DATETIME(3) NULL,
+    `email_verified` DATETIME(3) NULL,
     `username` VARCHAR(100) NULL,
     `is_guest` BOOLEAN NOT NULL DEFAULT false,
-    `isProfileSetup` BOOLEAN NOT NULL DEFAULT false,
+    `is_profile_setup` BOOLEAN NOT NULL DEFAULT false,
     `image` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
