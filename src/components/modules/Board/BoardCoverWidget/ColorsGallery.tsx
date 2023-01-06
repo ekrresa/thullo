0b6ccd94cf5fd0bbda2a9041ca4d559782499844
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-01-06 11:22:03 +0100

    Re-organise colors

--- src/components/modules/Board/BoardCoverWidget/ColorsGallery.tsx
+++ src/components/modules/Board/BoardCoverWidget/ColorsGallery.tsx
@@ -4,45 +4,40 @@ interface ColorsGalleryProps {
   selectCover: (input: string) => void;
 }
 
-//TODO: Add color picker
 export function ColorsGallery({ selectCover }: ColorsGalleryProps) {
   return (
     <div className="grid grid-cols-colors gap-2">
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-sky-600 hover:opacity-70 focus:ring-2 focus:ring-sky-600 focus:ring-offset-1"
-        onClick={() => selectCover('rgb(2 132 199)')}
+        className="h-28 cursor-pointer rounded-lg bg-[rgb(61,117,186)] hover:ring-2 hover:ring-[#3d75ba] hover:ring-offset-1"
+        onClick={() => selectCover('rgb(61,117,186)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-amber-500 hover:opacity-70 focus:ring-2 focus:ring-amber-500 focus:ring-offset-1"
-        onClick={() => selectCover('rgb(245 158 11)')}
-      ></Button>
-      <Button
-        className="h-28 cursor-pointer rounded-lg bg-emerald-500 hover:opacity-70 focus:ring-2 focus:ring-emerald-500 focus:ring-offset-1"
+        className="h-28 cursor-pointer rounded-lg bg-emerald-500 hover:ring-2 hover:ring-emerald-500 hover:ring-offset-1"
         onClick={() => selectCover('rgb(16 185 129)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-red-500 hover:opacity-70 focus:ring-2 focus:ring-red-500 focus:ring-offset-1"
+        className="h-28 cursor-pointer rounded-lg bg-red-500 hover:ring-2 hover:ring-red-500 hover:ring-offset-1"
         onClick={() => selectCover('rgb(239 68 68)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-purple-600 hover:opacity-70 focus:ring-2 focus:ring-purple-600 focus:ring-offset-1"
+        className="h-28 cursor-pointer rounded-lg bg-purple-600 hover:ring-2 hover:ring-purple-600 hover:ring-offset-1"
         onClick={() => selectCover('rgb(147 51 234)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-fuchsia-600 hover:opacity-70 focus:ring-2 focus:ring-fuchsia-600 focus:ring-offset-1"
+        className="h-28 cursor-pointer rounded-lg bg-fuchsia-600 hover:ring-2 hover:ring-fuchsia-600 hover:ring-offset-1"
         onClick={() => selectCover('rgb(192 38 211)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-teal-500 hover:opacity-70 focus:ring-2 focus:ring-teal-500 focus:ring-offset-1"
-        onClick={() => selectCover('rgb(20 184 166)')}
+        className="h-28 cursor-pointer rounded-lg bg-[rgb(249,139,7)] hover:ring-2 hover:ring-[rgb(249,139,7)] hover:ring-offset-1"
+        onClick={() => selectCover('rgb(249,139,7)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-cyan-600 hover:opacity-70 focus:ring-2 focus:ring-cyan-600 focus:ring-offset-1"
-        onClick={() => selectCover('rgb(8 145 178)')}
+        className="h-28 cursor-pointer rounded-lg bg-[rgb(250,57,114)] hover:ring-2 hover:ring-[rgb(250,57,114)] hover:ring-offset-1"
+        onClick={() => selectCover('rgb(250,57,114)')}
       ></Button>
       <Button
-        className="h-28 cursor-pointer rounded-lg bg-slate-500 hover:opacity-70 focus:ring-2 focus:ring-slate-500 focus:ring-offset-1"
-        onClick={() => selectCover('rgb(100 116 139)')}
+        className="h-28 cursor-pointer rounded-lg bg-[#05c7b7] hover:ring-2 hover:ring-[#05c7b7] hover:ring-offset-1"
+        onClick={() => selectCover('#05c7b7')}
       ></Button>
     </div>
   );
