df9486b2e63ea1f6313176411dd1de6a05476748
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-01-06 10:21:27 +0100

    Fix auth middleware

--- src/lib/middleware/with-authentication.ts
+++ src/lib/middleware/with-authentication.ts
@@ -1,17 +1,20 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth';
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
 import { authOptions } from '@lib/nextAuthConfig';
 
-export function withAuthentication(handler: NextApiHandler) {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
-    const session = await unstable_getServerSession(req, res, authOptions);
+const middleware = nextConnect<NextApiRequest, NextApiResponse>({ attachParams: true });
 
-    if (!session) {
-      return res.status(401).json({ error: 'Unauthorized action' });
-    }
+middleware.use(async (req, res, next) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
 
-    req.session = session;
+  if (!session) {
+    return res.status(401).json({ error: 'Unauthorized action' });
+  }
 
-    return handler(req, res);
-  };
-}
+  req.session = session;
+
+  next();
+});
+
+export const withAuthentication = middleware;
--- src/pages/api/boards/index.ts
+++ src/pages/api/boards/index.ts
@@ -1,53 +1,59 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
 import { v2 as cloudinary } from 'cloudinary';
 
 import { db } from '@lib/prisma';
-import { withMethods } from '@lib/middleware/with-methods';
 import { BoardCreateSchema } from '@models/board';
 import { parseError } from '@lib/utils';
 import { withAuthentication } from '@lib/middleware/with-authentication';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    // fetch all boards
-  }
+const requestHandler = nextConnect<NextApiRequest, NextApiResponse>();
 
-  if (req.method === 'POST') {
-    try {
-      const requestBody = BoardCreateSchema.parse(req.body);
+requestHandler.use(withAuthentication).post(async (req, res) => {
+  try {
+    const requestBody = BoardCreateSchema.parse(req.body);
 
-      const userId = req.session.user.id;
+    const userId = req.session.user.id;
 
-      const existingBoard = await db.board.findFirst({
-        where: { userId, title: requestBody.title },
-      });
+    const existingBoard = await db.board.findFirst({
+      where: { userId, title: requestBody.title },
+    });
 
-      if (existingBoard) {
-        throw new Error(`Board with ${requestBody.title} already exists`);
-      }
-
-      if (requestBody.image) {
-        const uploadResponse = await cloudinary.uploader.upload(requestBody.image, {
-          folder: 'thullo/boards',
-          eager: { aspect_ratio: '16:9', quality: 80 },
-        });
-
-        requestBody.image = uploadResponse.secure_url;
-      }
-
-      const newBoard = await db.board.create({
-        data: { ...requestBody, userId },
-      });
-
-      res.status(200).json({ data: newBoard });
-    } catch (error) {
-      const message = parseError(error);
-      res.status(400).json({ error: message });
+    if (existingBoard) {
+      throw new Error(`Board with ${requestBody.title} already exists`);
     }
-  }
-}
 
-export default withMethods(['GET', 'POST'], withAuthentication(handler));
+    if (requestBody.image) {
+      const uploadResponse = await cloudinary.uploader.upload(requestBody.image, {
+        folder: 'thullo/boards',
+        eager: { aspect_ratio: '16:9', quality: 80 },
+      });
+
+      requestBody.image = uploadResponse.secure_url;
+    }
+
+    const newBoard = await db.board.create({
+      data: { ...requestBody, userId },
+    });
+
+    res.status(200).json({ data: newBoard });
+  } catch (error) {
+    const message = parseError(error);
+    res.status(400).json({ error: message });
+  }
+});
+
+requestHandler.get(async (req, res) => {
+  try {
+    const boards = await db.board.findMany();
+    res.status(200).json({ data: boards });
+  } catch (error) {
+    const message = parseError(error);
+    res.status(400).json({ error: message });
+  }
+});
+
+export default requestHandler;
 
 export const config = {
   api: {
