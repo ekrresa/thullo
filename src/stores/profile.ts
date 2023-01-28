f29e94ca0c457b451b584d96a717fa57729a1d6b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2023-01-28 20:42:52 +0100

    Integrate redux devtools

--- src/stores/profile.ts
+++ src/stores/profile.ts
@@ -1,14 +1,17 @@
-import { User } from '@models/index';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { User } from '@models/index';
 
 interface ProfileState {
   info: User | null;
   updateProfile: (user: User | null) => void;
 }
 
-export const useProfileStore = create<ProfileState>(set => ({
-  info: null,
-  updateProfile: user => {
-    set({ info: user });
-  },
-}));
+export const useProfileStore = create<ProfileState>()(
+  devtools(set => ({
+    info: null,
+    updateProfile: user => {
+      set({ info: user });
+    },
+  }))
+);
